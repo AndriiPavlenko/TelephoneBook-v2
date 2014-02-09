d9d1ec41c02df43d5221069afe3ca47d721b3bd9
woolfer <woolfer2009@gmail.com>
2014-02-09 14:15:38 +0200

    new file:   build/classes/com/woolfer/telephonebook/ContactAddition.class 	new file:   build/classes/com/woolfer/telephonebook/ContactModification.class 	new file:   build/classes/com/woolfer/telephonebook/filters/Filter.class 	new file:   build/classes/com/woolfer/telephonebook/test/ForTesting.class 	new file:   src/com/woolfer/telephonebook/ContactAddition.java 	new file:   src/com/woolfer/telephonebook/ContactModification.java 	new file:   src/com/woolfer/telephonebook/filters/Filter.java 	new file:   src/com/woolfer/telephonebook/test/ForTesting.java

---
+++ src/com/woolfer/telephonebook/ContactModification.java
@@ -0,0 +1,25 @@
+package com.woolfer.telephonebook;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ContactModification extends HttpServlet {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		String actionCh = request.getParameter("Change");
+		String actionDel = request.getParameter("Delete");
+		
+		if(actionCh != null) {
+			RequestDispatcher view = request.getRequestDispatcher("ChangeRec.jsp");
+			view.forward(request, response);
+		} else if (actionDel != null) {
+			RequestDispatcher view = request.getRequestDispatcher("isDelete.jsp");
+			view.forward(request, response);
+		}
+		
+	}
+}
